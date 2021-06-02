026e6687cdfc14bf0e9cd202345cf1b16efb0f93
Gregory Spraggins <gregoryspraggins@MacBook-Pro-7.fios-router.home>
2021-06-02 14:11:07 -0400

    Adding temporarily hardcoded introduction

---
+++ template-parts/components/intro.php
@@ -0,0 +1,14 @@
+<!-- Begin Intro -->
+<section class="intro section section--brush">
+  <div class="row row-site column pt4 pb0">
+    <div class="row medium-up-2 text-grid-col text-grid-col--2">
+      <div class="columns">
+        <h2>What is<br>True North?</h2>
+      </div>
+      <div class="columns">
+        <p>Dare Northward began as an idea. It has become a rallying cry and a movement. While we celebrate what has been accomplished in this audacious campaign, the most rewarding part of the climb awaits. Join us as we reach untold heights. #darenorthward</p>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- End Intro -->
